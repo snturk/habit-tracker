5ce10c430dc352c64d81031ac31f17c095ae259a
Muratcan Şentürk <muratcan.senturk@ozguryazilim.com.tr>
2023-07-11 18:48:41 +0300

    feat: UI Changes
    * Added safe area
    
    Refs: #12

--- app/utils/globalStyles.ts
+++ app/utils/globalStyles.ts
@@ -3,7 +3,6 @@ import { StyleSheet } from "react-native";
 const globalStyles = StyleSheet.create({
 	stdContainer: {
 		flex: 1,
-		height: "100%",
 		backgroundColor: "#242426",
 		color: '#FFFFFF',
 		flexDirection: "column",
